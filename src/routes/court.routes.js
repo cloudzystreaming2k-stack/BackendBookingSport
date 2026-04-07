84a5e5ad26c76d206fe7f86a020cbdd388f6b1f3
cloudzy <cloudzystreaming2k@gmail.com>
2026-04-07 13:38:36 +0700

    Sửa route cho userlogin vẫn vào tìm được sân

--- src/routes/court.routes.js
+++ src/routes/court.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { getCourts, getCourtById, getCourtSlotsByDate } from '../controllers/court.controller.js';
+import { getAllCourtTypes } from '../controllers/courtType.controller.js';
+import { getAllFacilities } from '../controllers/facility.controller.js';
 
 const router = express.Router();
 
 router.get('/', getCourts);
-router.get('/:id/slots', getCourtSlotsByDate); // Phải đặt TRƯỜC /:id
+router.get('/types', getAllCourtTypes);
+router.get('/facilities', getAllFacilities);
+router.get('/:id/slots', getCourtSlotsByDate); // Phải đặt TRƯỚC /:id
 router.get('/:id', getCourtById);
 
 export default router;
